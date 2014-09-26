d6ae277f76596b53195d7f6a3cad864a7e79d244
31449530 <31449530@087865-P33S105.macklabs.br>
2014-09-26 10:34:01 -0300

    falta mostrar as variaveis no documento cadastra.php

--- cadastro.php
+++ cadastro.php
@@ -1,10 +1,19 @@
-<?php
-	$nome = $_POST["nome"];
-	$email = $_POST["email"];
+<!DOCTYPE html>
+    <html>
+        <head>
+            <title> Produto </title>
+            <meta charset="UTF-8">
+        </head>	
+            <body>
+                <?php
+                    $nome = $_POST["nome"];
+                    $email = $_POST["email"];
 	
-	$id = $nome; // Suponha que id foi gerado aleatoriamente. Poderia ser um número.
-	
-	echo "Voce está cadastrado. Chame os seus amigos e ganhe pontos.";
-	echo "Para isso, é só passar o link abaixo:";
-	echo "http://www.supersite.com.br/index.php?id=". $id; 
-?>
+                    $id = $nome; // Suponha que id foi gerado aleatoriamente. Poderia ser um número.
+        
+                    echo "Voce está cadastrado. Chame os seus amigos e ganhe pontos.";
+                    echo "Para isso, é só passar o link abaixo:";
+                    echo "http://www.supersite.com.br/index.php?id=". $id; 
+                   ?>
+            </body>
+    </html>
