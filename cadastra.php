92140c0baae96d169874ca0dd684f7a58c6868ae
Patricia <Patricia@Patricia-VAIO>
2014-09-28 19:58:16 -0300

    validação do formulario feita, e exibição das variaveis concluida

--- cadastra.php
+++ cadastra.php
@@ -5,31 +5,30 @@
             <meta charset="UTF-8">
         </head>	
             <body>
-                <form method="POST" action="cadastro.php">
-                <?php
-                 $nome = $_POST["nome"];
-                 $email = $_POST["email"];
-                 $datanascimento = $_POST["datanascimento"];
-                 $sexo = $_POST["sexo"];
-                 $profissao = $_POST["profissao"];
-                 $telefone = $_POST["telefone"];
-                 $endereco= $_POST["endereco"];
-                 $cidade = $_POST["cidade"];
-                 $estado = $_POST["estado"];
-                 $cep = $_POST["cep"];
-                 
-                 echo "seu nome é: ".$nome."<br/>";
-                 echo "seu email é: ".$email."<br/>";
-                 echo "sua data de nascimento é: ".$datanascimento."<br/>";
-                 echo "seu sexo é: ".$sexo."<br/>";
-                 echo "sua profissão é: ".$profissao."<br/>";
-                 echo "seu telefone é: ".$telefone."<br/>";
-                 echo "seu endereço é: ".$endereco."<br/>";
-                 echo "sua cidade é: ".$cidade."<br/>";
-                 echo "seu estado é: ".$estado."<br/>";
-                 echo "seu cep é: ".$cep."<br/>";
-                
-                ?>
-            </form>
+                <form method="POST" action="cadastro.php" >
+                    
+                    <h3> Confira seus dados e finalize o processo de cadastramento </h3>
+                        
+                    <?php $username = $_POST['username']; if(strlen($username)<6 OR empty($username))
+                    {echo " Verifique se o campo do Username esta preenchido, ou se ele tem menos de 6 caracteres.<br/>";}?>
+                    
+                    <?php $senha = $_POST['senha']; if (strlen($senha)<5 OR empty($senha)) 
+                    {echo " Verifique se o campo Senha esta preenchido, ou se a senha tem menos de 5 digitos.<br/>";}?>
+                    
+                        <?php
+                             echo "seu nome é: ".$_POST['nome']."<br/>";
+                             echo "seu email é: ".$_POST['email']."<br/>";
+                             echo "sua data de nascimento é: ".$_POST ['datanascimento']."<br/>";
+                             echo "seu sexo é: ".$_POST['sexo']."<br/>";
+                             echo "sua profissão é: ".$_POST['profissao']."<br/>";
+                             echo "seu telefone é: ".$_POST['telefone']."<br/>";
+                             echo "seu endereço é: ".$_POST['endereco']."<br/>";
+                             echo "sua cidade é: ".$_POST['cidade']."<br/>"; 
+                             echo "seu estado é: ".$_POST['estado']."<br/>";
+                             echo "seu cep é: ".$_POST['cep']."<br/>";
+                             echo "seu Username é: ".$_POST['username']."<br/>";
+                        ?>
+                    <input type="submit" value="finalizar processo"/>
+                </form>
             </body>
     </html>
--- etapa1.php
+++ etapa1.php
@@ -1,52 +1,60 @@
-<html>
-<head> 
-	<title>Desenvolvendo Websites com PHP</title> 
-	<meta charset="UTF-8">
-</head>
-<body> 
-	<p align="center"><big><big>
-		<strong>Cadastramento - Etapa 1 de 3</strong></big></big>
-	</p> 
+<!DOCTYPE html>
+    <html>
+        <head> 
+            <title>Desenvolvendo Websites com PHP</title> 
+            <meta charset="UTF-8">
+        </head>
+            <body> 
+                <p align="center"><big><big>
+                    <strong>Cadastramento - Etapa 1 de 3</strong></big></big>
+                </p> 
 	
-	<form method="POST" action="etapa2.php"> 
- 		<div align="center">
-			<center> 
-				<p>Nome: <input type="text" name="nome" size="20"></p>
-			</center>
-		</div>
-		<div align="center">
-			<center> 
-				<p>E-mail: <input type="text" name="email" size="20"></p> 
- 			</center>
-		</div>
-		<div align="center">
-			<center> 
-				<p>Data de nascimento: <input type="text" name="datanascimento" size="20"></p> 
- 			</center>
-		</div>
-		<div align="center">
-			<center> 
+                <form method="POST" action="etapa2.php"> 
+                    <div align="center">
+                            <center> 
+                                <p>Nome: <input type="text" name="nome" size="20" value="<?php echo $_POST['nome']?>"></p>
+                                <?php $nome = $_POST['nome']; if(empty($nome)){echo "Verifique se o campo nome esta preenchido.<br>"; $erro=TRUE;}?>
+                            </center>
+                    </div>
+		
+                    <div align="center">
+                            <center> 
+                                <p>E-mail: <input type="text" name="email" size="20" value="<?php echo $_POST ['email'] ?>"></p>
+                                <?php $email = $_POST['email']; if(empty($email) OR !strstr($email,'@')){echo "Digite o e-mail corretamente ou verifique se o campo estra preenchido. <br>"; $erro=TRUE;} ?>
+                            </center>
+                    </div>
+		
+                    <div align="center">
+                            <center> 
+                                <p>Data de nascimento: <input type="text" name="datanascimento" size="20"></p>
+                            </center>
+                    </div>
+		
+                    <div align="center">
+                            <center> 
 				<p> 
-					Sexo: 
-					<input type="radio" value="masculino" checked name="sexo">Masculino&nbsp;&nbsp; 
-					<input type="radio" name="sexo" value="feminino">Feminino
+                                    Sexo: 
+                                    <input type="radio" value="masculino" checked name="sexo">Masculino&nbsp;&nbsp; 
+                                    <input type="radio" name="sexo" value="feminino">Feminino
 				</p> 
- 			</center>
-		</div>
-		<div align="center">
-			<center> 
+                            </center>
+                    </div>
+		
+                    <div align="center">
+                            <center> 
 				<p>
-					Profissão: <input type="text" name="profissao" size="20">
+                                    Profissão: <input type="text" name="profissao" size="20">
 				</p> 
- 			</center>
-		</div>
-		<div align="center">
-			<center>
+                            </center>
+                    </div>
+		
+                    <div align="center">
+                            <center>
 				<p>	
-					<input type="submit" value="Prosseguir &gt;&gt;" name="prosseguir">
+                                    <input type="submit" value="Prosseguir &gt;&gt;" name="prosseguir">
 				</p> 
- 			</center>
-		</div> 
-	</form> 
-</body> 
-</html> 
+                            </center>
+                    </div> 
+                </form> 
+            </body> 
+    </html> 
