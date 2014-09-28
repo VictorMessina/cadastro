7bb0ee0df3f82fd5026a94f865e004793904bf95
Patricia <Patricia@Patricia-VAIO>
2014-09-28 19:59:20 -0300

    modificações feitas

--- cadastro.php
+++ cadastro.php
@@ -6,14 +6,14 @@
         </head>	
             <body>
                 <?php
-                    $nome = $_POST["nome"];
-                    $email = $_POST["email"];
-	
+                    $nome = $_POST['nome'];
+                    $email = $_POST['email'];
+              
                     $id = $nome; // Suponha que id foi gerado aleatoriamente. Poderia ser um número.
         
                     echo "Voce está cadastrado. Chame os seus amigos e ganhe pontos.";
                     echo "Para isso, é só passar o link abaixo:";
-                    echo "http://www.supersite.com.br/index.php?id=". $id; 
+                    echo "localhost//cadastro/index.php?id=". $id; 
                    ?>
             </body>
     </html>
--- gerencia.php
+++ gerencia.php
@@ -1,17 +1,26 @@
-<?php
-	$operacao = $_POST["operacao"];
-	if (strcmp($operacao, "inclusao") == 0 ) {
-		// realiza a inclusao do produto
-		echo "Produto incluído";
-	}
-	
-	if (strcmp($operacao, "exclusao") == 0 ) {
-		// realiza a exclusao do produto
-		echo "Produto excluído";
-	}
-        
-	if (strcmp($operacao, "atualizar") == 0 ) {
-		// realiza a atualização do produto
-		echo "Produto atualizado";
-	}
-?>
+<!DOCTYPE html>
+    <html>
+        <head> 
+            <title>Gerencia.php</title> 
+            <meta charset="UTF-8">
+        </head>
+            <body> 
+                <?php
+                    $operacao = $_POST["operacao"];
+                    if (strcmp($operacao, "inclusao") == 0 ) {
+                            // realiza a inclusao do produto
+                            echo "Produto incluído";
+                    }
+
+                    if (strcmp($operacao, "exclusao") == 0 ) {
+                            // realiza a exclusao do produto
+                            echo "Produto excluído";
+                    }
+
+                    if (strcmp($operacao, "atualizar") == 0 ) {
+                            // realiza a atualização do produto
+                            echo "Produto atualizado";
+                    }
+                ?>
+            </body>
+    </html>
