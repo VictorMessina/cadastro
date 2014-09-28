051f3d8a95c54fcecacc3daa08ef86c217456ca6
Patricia <Patricia@Patricia-VAIO>
2014-09-28 20:01:27 -0300

    falta mudar a URL pedida

--- cadastro.php
+++ cadastro.php
@@ -11,6 +11,7 @@
               
                     $id = $nome; // Suponha que id foi gerado aleatoriamente. Poderia ser um número.
         
+                    
                     echo "Voce está cadastrado. Chame os seus amigos e ganhe pontos.";
                     echo "Para isso, é só passar o link abaixo:";
                     echo "localhost//cadastro/index.php?id=". $id; 
