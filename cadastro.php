3d06aff5c6b1df0e66992708bb2f599c97423ccb
31449530 <31449530@093166-p31s203.macklabs.br>
2014-09-29 12:03:39 -0300

    URL funcionando

--- cadastro.php
+++ cadastro.php
@@ -6,15 +6,10 @@
         </head>	
             <body>
                 <?php
-                    $nome = $_POST['nome'];
-                    $email = $_POST['email'];
-              
-                    $id = $nome; // Suponha que id foi gerado aleatoriamente. Poderia ser um número.
-        
-                    
+                    $id = $_POST['nome']; // Suponha que id foi gerado aleatoriamente. Poderia ser um número.
                     echo "Voce está cadastrado. Chame os seus amigos e ganhe pontos.";
                     echo "Para isso, é só passar o link abaixo:";
-                    echo "localhost//cadastro/index.php?id=". $id; 
+                    echo " http://localhost:8081//cadastro/index.php?id= ". $id; 
                    ?>
             </body>
     </html>
